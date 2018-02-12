b61a9718edd50cdb38b705ad2b76d0f264030838
Patel <t_patel64@tss-hr420-dm40.fc.ca>
2018-02-12 13:52:23 -0500

    adding database link stuff in server

---
+++ config.js
@@ -0,0 +1,10 @@
+const config = {
+  host : "localhost",
+  port : 8889,
+  user : "root",
+  password : "root",
+  database : "db_cooper_car"
+};
+
+
+module.exports = config;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,34 @@
 var express = require('express');
+var mysql = require('mysql');
+var connect = require('../utils/sqlconnect');
 var router = express.Router();
+
+const templateTypes = [
+  { name: "Jade", usefulness : "sucky" },
+  { name: "EJS", usefulness : " a little better" },
+  { name: "Handlebars", usefulness : "good" }
+]
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express', message : "I really Dont know about jade" });
+
+
+  connect.query('SELECT * FROM mainmodel', (err, result, fields) => {
+    if (err) {
+      throw err; console.log(err);
+    }else {
+      console.log(result);
+    }
+  });
+
+
+  res.render('home', {
+     title: 'Express',
+     message : "I really Dont know about EJS",
+     template : templateTypes,
+     anothermessage : "right now I am with handlebars"
+   });
 });
 
 module.exports = router;
